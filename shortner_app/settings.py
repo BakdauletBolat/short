d2669079ba5be4393129acada4e9f30be9fc13dd
BakdauletBolat <bakosh21345@gmail.com>
2022-08-05 17:24:36 +0600

    a

--- shortner_app/settings.py
+++ shortner_app/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:6500']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
